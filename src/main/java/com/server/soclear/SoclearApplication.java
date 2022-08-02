616498c48d6767cf3f95794cb8aa6e8bc93df24b
yunjinchoidev <yunjinchoidev@gmail.com>
2022-08-02 22:12:38 +0900

    최초 프로젝트 커밋

---
+++ src/main/java/com/server/soclear/SoclearApplication.java
@@ -0,0 +1,13 @@
+package com.server.soclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoclearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoclearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/soclear/SoclearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.soclear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoclearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
